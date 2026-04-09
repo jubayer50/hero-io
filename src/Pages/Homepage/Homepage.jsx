8ffa716af5a893b1b2c79b3a3d3f71db9214a85a
jubayer50 <aujubayer1@gmail.com>
2026-04-10 00:06:32 +0600

    Trusted by Millions design

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../Components/HomePage/Banner/Banner";
+import Status from "../../Components/HomePage/Status/Status";
 
 const Homepage = () => {
   return (
     <div>
       <Banner></Banner>
+      <Status></Status>
     </div>
   );
 };
